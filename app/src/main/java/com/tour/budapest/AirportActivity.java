555d42da8f72cda370cccc2c5fbc9bf1532ac789
Enrique Sanchez <esanchez.devel@gmail.com>
2024-10-14 20:28:31 +0200

    feat: show bus 100E route in map

--- app/src/main/java/com/tour/budapest/AirportActivity.java
+++ app/src/main/java/com/tour/budapest/AirportActivity.java
@@ -1,6 +1,8 @@
 package com.tour.budapest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,12 @@ public class AirportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_airport);
+
+        TextView textBus100ELink = findViewById(R.id.airport_bus_100e_link);
+
+        textBus100ELink.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Bus100ERouteActivity.class);
+            startActivity(intent);
+        });
     }
 }
